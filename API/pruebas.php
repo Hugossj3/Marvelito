19fa3d256a47626a0c82a393faad0e6270662347
Hugossj3 <gdhugo666@gmail.com>
2024-10-08 14:43:49 +0200

    no c, fumo?

--- API/pruebas.php
+++ API/pruebas.php
@@ -5,5 +5,12 @@ require_once("conexion.php");
 
 $a=dameConexion();
 
-$query="";
-$a->prepare($query);
+$query="SELECT img FROM personaje";
+$stm=$a->prepare($query);
+$result=$stm->get_result();
+
+if($result->num_rows>0){
+    while($personaje=$result->fetch_assoc()){
+        extract($personaje);
+    }
+}
