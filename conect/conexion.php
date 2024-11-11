988621bf30a1d69e5d39688cc72dc9d8a3f97df0
Hugossj3 <gdhugo666@gmail.com>
2024-11-11 10:45:40 +0100

    para subirlo

--- conect/conexion.php
+++ conect/conexion.php
@@ -2,7 +2,10 @@
 
 #Esto es lo que da la conexion al resto del codigo con la base de datos
 function dameConexion(){
-    $conn =mysqli_connect("localhost", "root", "", "marvelito");
+    $conn =mysqli_connect("PMYSQL177.dns-servicio.com:3306",
+	 "yooo", 
+	 "c9Cjh70!4",
+	  "10598388_marvelito");
 		
 	$conn->set_charset("utf8");
 		if ($conn->connect_errno) {
